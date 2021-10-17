cff5ad59c944b9e2b0c78768c6da7d1e1fe3318d
taemin-steve <okchrisok@naver.com>
2021-10-17 22:45:09 +0900

    make Tree adt

---
+++ Tree/Tree.py
@@ -0,0 +1,34 @@
+class Tree:
+    class Position:
+        def element(self):
+            raise NotImplementedError('must be implemented by subclass')
+        
+        def __eq__(self, other):
+            raise NotImplementedError('must be implemented by subclass')
+        
+        def __nq__(self,other):
+            return not(self == other)
+
+        def root(self):
+            raise NotImplementedError('must be implemented by subclass')
+
+        def parent(self, p):
+            raise NotImplementedError('must be implemented by subclass')
+
+        def num_children(self,p):
+            raise NotImplementedError('must be implemented by subclass')
+        
+        def children(self, p):
+            raise NotImplementedError('must be implemented by subclass')
+        
+        def __len__(self):
+            raise NotImplementedError('must be implemented by subclass')
+        
+        def is_root(self,p):
+            return.self.root() == p
+        
+        def is_leaf(self,p):
+            return self.num_childern(p) == 0
+        
+        def is_empty(self):
+            return len(self) == 0
